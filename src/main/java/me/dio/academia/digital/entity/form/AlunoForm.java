3959fca69e5a9131e0953b2a93127675c94bd7e3
Jose Carlos Feitosa <92383795+feitosadev@users.noreply.github.com>
2023-04-23 16:55:41 -0300

    Add files via upload

--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -2,6 +2,13 @@ package me.dio.academia.digital.entity.form;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +19,20 @@ import lombok.NoArgsConstructor;
 
 public class AlunoForm {
 
+	@NotBlank(message = "Preencha o campo corretamente")
+	@Size(min = 3, max = 50, message = "'{validatedValue}' precisa estar entre {min} e {max} caracteres")
 	private String nome;
 
+	@NotBlank
+	@CPF(message = "'$validatedValue}', é invalido!")
 	private String cpf;
 
+	@NotBlank
+	@Size(min = 3, max = 50, message = "'{validatedValue}' precisa estar entre {min} e {max} caracteres")
 	private String bairro;
 
+	@NotNull(message = "Preencha o campo corretamente.")
+	@Past(message = "Data '${validatedValue}' é invalida.")
 	private LocalDate dataDeNascimento;
 
 	public String getNome() {
